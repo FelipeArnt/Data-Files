f68f979b066c7ead6a8191fd0e4770d12b6073ac
Felipe Arnt <102182726+FelipeArnt@users.noreply.github.com>
2025-03-22 14:35:42 -0300

    Upload dos arquivos iniciais
    Arquivo  "nob.h" importado de "https://github.com/tsoding/musializer"

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#define NOB_IMPLEMENTATION
+#include "nob.h"
+
+
+static bool map[256][256] = {0};
+
+int main(void)
+{
+	const char *caminho_arquivo = "a.png";
+	Nob_String_Builder conteudo = {0};
+	if(!nob_read_entire_file(caminho_arquivo, &conteudo )) return 1;
+
+
+	if(conteudo.count> 0){
+		for(size_t i = 0; i < conteudo.count; i++){
+			uint8_t a = conteudo.items[i];
+			uint8_t b = conteudo.items[i+1];
+		}
+	}
+
+	nob_log(NOB_INFO,"Arquivo %s [%zu bytes]", caminho_arquivo, conteudo.count);
+	return 0;
+}
+
